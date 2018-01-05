7b2a41b608c3a4e0d9ee36362ccc76d39c0babaf
Qccccc's laptop <896853205@qq.com>
2018-01-06 01:20:53 +0800

    修改数据库数据以及配置

--- src/main/resources/dbconfig.js
+++ src/main/resources/dbconfig.js
@@ -2,14 +2,14 @@
  * @Author:qc 
  * @Date: 2017-12-26 19:04:05 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2017-12-27 15:47:03
+ * @Last Modified time: 2018-01-06 01:19:52
  */
 
 let connection = {
     host:'localhost',
     user:'root',
     password:'a896',
-    database:'survey',
+    database:'disaster',
     port:3306
 };
 
--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -4,162 +4,157 @@ Navicat MySQL Data Transfer
 Source Server         : myself
 Source Server Version : 50528
 Source Host           : localhost:3306
-Source Database       : survey
+Source Database       : disaster
 
 Target Server Type    : MYSQL
 Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2018-01-02 22:56:04
+Date: 2018-01-06 01:19:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
--- Table structure for `b_answer`
+-- Table structure for `b_earthquake`
 -- ----------------------------
-DROP TABLE IF EXISTS `b_answer`;
-CREATE TABLE `b_answer` (
-  `id` char(32) NOT NULL,
-  `duration` varchar(255) DEFAULT NULL,
-  `dt` date DEFAULT NULL,
-  `score` varchar(255) DEFAULT NULL,
-  `inquiry_id` char(32) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of b_answer
--- ----------------------------
-
--- ----------------------------
--- Table structure for `b_answer_detial`
--- ----------------------------
-DROP TABLE IF EXISTS `b_answer_detial`;
-CREATE TABLE `b_answer_detial` (
-  `id` char(32) NOT NULL,
-  `answer_id` char(32) DEFAULT NULL,
-  `solution` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of b_answer_detial
--- ----------------------------
-
--- ----------------------------
--- Table structure for `b_comment`
--- ----------------------------
-DROP TABLE IF EXISTS `b_comment`;
-CREATE TABLE `b_comment` (
-  `id` char(32) NOT NULL,
-  `begin_score` varchar(255) DEFAULT NULL,
-  `end_score` varchar(255) DEFAULT NULL,
-  `remark` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of b_comment
--- ----------------------------
-
--- ----------------------------
--- Table structure for `b_inquiry`
--- ----------------------------
-DROP TABLE IF EXISTS `b_inquiry`;
-CREATE TABLE `b_inquiry` (
-  `id` char(32) NOT NULL,
-  `user_id` char(32) DEFAULT NULL,
-  `switch` char(1) DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `create_time` date DEFAULT NULL,
-  `end_time` date DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `state` char(1) DEFAULT NULL COMMENT '处理状态',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of b_inquiry
--- ----------------------------
-
--- ----------------------------
--- Table structure for `b_opation`
--- ----------------------------
-DROP TABLE IF EXISTS `b_opation`;
-CREATE TABLE `b_opation` (
-  `id` char(32) NOT NULL,
-  `symbol` varchar(255) DEFAULT NULL,
-  `question_id` char(32) DEFAULT NULL,
-  `content` varchar(32) DEFAULT NULL,
-  `score` int(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of b_opation
--- ----------------------------
-
--- ----------------------------
--- Table structure for `b_question`
--- ----------------------------
-DROP TABLE IF EXISTS `b_question`;
-CREATE TABLE `b_question` (
-  `id` char(32) NOT NULL,
-  `inquiry_id` char(32) DEFAULT NULL,
-  `num` int(255) DEFAULT NULL,
-  `type_id` varchar(32) DEFAULT NULL,
-  `content` varchar(255) DEFAULT NULL,
-  `essential` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of b_question
--- ----------------------------
-
--- ----------------------------
--- Table structure for `b_question_type`
--- ----------------------------
-DROP TABLE IF EXISTS `b_question_type`;
-CREATE TABLE `b_question_type` (
-  `id` varchar(32) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of b_question_type
--- ----------------------------
-
--- ----------------------------
--- Table structure for `s_role`
--- ----------------------------
-DROP TABLE IF EXISTS `s_role`;
-CREATE TABLE `s_role` (
-  `id` char(3) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of s_role
--- ----------------------------
-
--- ----------------------------
--- Table structure for `s_user`
--- ----------------------------
-DROP TABLE IF EXISTS `s_user`;
-CREATE TABLE `s_user` (
+DROP TABLE IF EXISTS `b_earthquake`;
+CREATE TABLE `b_earthquake` (
   `id` char(32) NOT NULL COMMENT '编号',
-  `account` varchar(255) DEFAULT NULL COMMENT '账号',
-  `password` varchar(255) DEFAULT NULL COMMENT '密码',
-  `role_id` char(3) DEFAULT NULL COMMENT '权限',
-  `name` varchar(255) DEFAULT NULL COMMENT '姓名',
-  `portrait` varchar(255) DEFAULT NULL COMMENT '头像路径',
+  `area_id` char(32) DEFAULT NULL COMMENT '地区编号',
+  `collapse` float(255,0) DEFAULT NULL COMMENT '倒塌系数',
+  `strength_id` char(32) DEFAULT NULL COMMENT '烈度id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='地震信息表';
 
 -- ----------------------------
--- Records of s_user
+-- Records of b_earthquake
 -- ----------------------------
-INSERT INTO `s_user` VALUES ('1', '896853205', 'a896853205', '001', '钱程', null);
+
+-- ----------------------------
+-- Table structure for `b_need_result`
+-- ----------------------------
+DROP TABLE IF EXISTS `b_need_result`;
+CREATE TABLE `b_need_result` (
+  `id` char(32) NOT NULL COMMENT '编号',
+  `earthquake_id` char(32) DEFAULT NULL COMMENT '地震编号',
+  `area_id` char(32) DEFAULT NULL COMMENT '地区编号',
+  `good_id` char(32) DEFAULT NULL COMMENT '物资编号',
+  `amout` float(255,0) DEFAULT NULL COMMENT '数量',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='需求信息表';
+
+-- ----------------------------
+-- Records of b_need_result
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `d_area`
+-- ----------------------------
+DROP TABLE IF EXISTS `d_area`;
+CREATE TABLE `d_area` (
+  `id` char(32) NOT NULL COMMENT '编号',
+  `name` varchar(255) DEFAULT NULL COMMENT '地区名',
+  `population` float(255,0) DEFAULT NULL COMMENT '人口总数',
+  `density` float(255,0) DEFAULT NULL COMMENT '人口密度',
+  `longitude` varchar(255) DEFAULT NULL COMMENT '经度',
+  `latitude` varchar(255) DEFAULT NULL COMMENT '纬度',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='地区基本情况表';
+
+-- ----------------------------
+-- Records of d_area
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `d_density_factor`
+-- ----------------------------
+DROP TABLE IF EXISTS `d_density_factor`;
+CREATE TABLE `d_density_factor` (
+  `id` char(32) NOT NULL COMMENT '编号',
+  `begin` float(255,0) DEFAULT NULL COMMENT '下限',
+  `end` float(255,0) DEFAULT NULL COMMENT '上限',
+  `coeffcient` float(255,0) DEFAULT NULL COMMENT '系数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='人口密度系数表';
+
+-- ----------------------------
+-- Records of d_density_factor
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `d_good`
+-- ----------------------------
+DROP TABLE IF EXISTS `d_good`;
+CREATE TABLE `d_good` (
+  `id` char(32) NOT NULL COMMENT '编号',
+  `name` varchar(255) DEFAULT NULL COMMENT '物资名',
+  `type_id` char(3) DEFAULT NULL COMMENT '种类id',
+  `size` varchar(255) DEFAULT NULL COMMENT '规格',
+  `unit` varchar(255) DEFAULT NULL COMMENT '单位',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='应急物资表';
+
+-- ----------------------------
+-- Records of d_good
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `d_need_factor`
+-- ----------------------------
+DROP TABLE IF EXISTS `d_need_factor`;
+CREATE TABLE `d_need_factor` (
+  `id` char(32) NOT NULL COMMENT '编号',
+  `area_id` char(32) DEFAULT NULL COMMENT '地区编号',
+  `good_id` char(32) DEFAULT NULL COMMENT '物资编号',
+  `coeffcient` float(255,0) DEFAULT NULL COMMENT '系数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='地区需求系数表';
+
+-- ----------------------------
+-- Records of d_need_factor
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `d_strength_factor`
+-- ----------------------------
+DROP TABLE IF EXISTS `d_strength_factor`;
+CREATE TABLE `d_strength_factor` (
+  `id` char(32) NOT NULL COMMENT '编号',
+  `level` varchar(255) DEFAULT NULL COMMENT '等级',
+  `coeffcient` float(255,0) DEFAULT NULL COMMENT '系数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='烈度系数表';
+
+-- ----------------------------
+-- Records of d_strength_factor
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `d_type`
+-- ----------------------------
+DROP TABLE IF EXISTS `d_type`;
+CREATE TABLE `d_type` (
+  `id` char(32) NOT NULL COMMENT '编号',
+  `name` varchar(255) DEFAULT NULL COMMENT '种类名',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='种类表';
+
+-- ----------------------------
+-- Records of d_type
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `d_type_factor`
+-- ----------------------------
+DROP TABLE IF EXISTS `d_type_factor`;
+CREATE TABLE `d_type_factor` (
+  `id` char(32) NOT NULL COMMENT '编号',
+  `area_id` char(32) DEFAULT NULL COMMENT '地区编号',
+  `type_id` char(3) DEFAULT NULL COMMENT '种类id',
+  `coeffcient` float(255,0) DEFAULT NULL COMMENT '系数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='需求种类系数表';
+
+-- ----------------------------
+-- Records of d_type_factor
+-- ----------------------------
