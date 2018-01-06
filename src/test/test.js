98ff6f826fab7e3193a5dc73c3a2cf84e7aee830
Qccccc's laptop <896853205@qq.com>
2018-01-06 19:20:45 +0800

    修改成增加100个灾区地点

--- src/test/test.js
+++ src/test/test.js
@@ -2,12 +2,16 @@
  * @Author: qc
  * @Date: 2018-01-06 12:41:18 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-01-06 18:47:44
+ * @Last Modified time: 2018-01-06 19:20:10
  */
 let assert = require('assert');
 let area = require('./webapp/area');
+// 插入100个灾区
 describe('灾区', () => {
   it('灾区插入', (done) => {
-    area(done);
+    for(let i = 10;i < 100;i++){
+      area('10'+i);
+    }
+    done();
   });
 });
--- src/test/webapp/area.js
+++ src/test/webapp/area.js
@@ -2,22 +2,22 @@
  * @Author: qc
  * @Date: 2018-01-06 17:56:20 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-01-06 19:01:50
+ * @Last Modified time: 2018-01-06 19:14:39
  */
 let random = require('../app/common/random');
 let axios = require('axios');
 let baseUrl = 'http://localhost:3000';
-let insertParam = {
-  name: '1001',
-  population: random(120000000, 130000000),
-  density: random(30, 800),
-  longitude:'97°21′',
-  latitude:'26°03′'
-};
 // 发起增加灾点基本情况请求
-module.exports = function insert(done){
+module.exports = function insert(name){
+  let insertParam = {
+    name: '1001',
+    population: random(120000000, 130000000),
+    density: random(30, 800),
+    longitude:'97°21′',
+    latitude:'26°03′'
+  };
+  insertParam.name = name;
   axios.post(`${baseUrl}/test/area/insert`,insertParam).then((res)=>{
-    done();
   }).catch((e)=>{
     console.log(e);
   });
